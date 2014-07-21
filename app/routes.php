67c099937c6326a5f896768c60583aa3edf91041
Rachel Loziuk <rachel.loziuk@gmail.com>
2014-07-21 12:28:47 -0500

    added validation to file uploading

--- app/routes.php
+++ app/routes.php
@@ -28,10 +28,16 @@ Route::post('userform', function(){
     'username' => 'required|min:6',
     'password' => 'required|same:password_confirm'
   );
+
+  // VALIDATION:  
+  // Validator class (ships with Laravel) - the first argument passed to the make method is the data under validation. The second argument contains the validation rules that should be applied to the data.
   $validation = Validator::make(Input::all(), $rules);
   if ($validation->fails()){
     return Redirect::to('userform')->withErrors($validation)->withInput();
   } 
+  // Another way to retrieve the error messages from the validator: $messages = $validator->messages();
+  // Notice that we do not have to explicitly bind the error messages to the view in our GET route. This is because Laravel will always check for errors in the session data, and automatically bind them to the view if they are available. So, it is important to note that an $errors variable will always be available in all of your views, on every request, allowing you to conveniently assume the $errors variable is always defined and can be safely used. The $errors variable will be an instance of MessageBag.
+
   return Redirect::to('userresults') -> withInput();
 });
 
@@ -45,14 +51,23 @@ Route::get('fileform', function(){
   return View::make('fileform');
 });
 
-// POSTS FORM INPUT - FILES:
+// POSTS FORM INPUT & VALIDATE - FILES:
 // Saves file with a different filename.
+// Move the file to its permanent location using the file's move() method, with the first parameter being the directory where we will save the file; the second is the new name of the file.
 Route::post('fileform', function(){
-  $file = Input::file('myfile');
-  $ext = $file->guessExtension();
-  if ($file->move('files', 'newfilename.' . $ext)) {
-    return 'Success';
+  $rules = array(
+    'myfile' => 'mimes:doc, pdf, jpg|max: 1000',
+    'myfile' => 'image'
+  );
+  $validation = Validator::make(Input::all(), $rules);
+  if ($validation->fails()){
+    return Redirect::to('fileform') ->withErrors($validation)->withInput();
   } else {
-    return 'Error';
+    $file = Input::file('myfile');
+    if ($file->move('files', $file ->getClientOriginalName())) {
+      return "Success";
+    } else {
+      return "Error";
+    }
   }
 });
--- app/views/fileform.blade.php
+++ app/views/fileform.blade.php
@@ -2,11 +2,17 @@
 @section('content')
 <h3>Howdy. Here is our form:</h3>
 <h2>File Upload:</h2>
-<!-- CREATE A FORM: -->
-<!-- 'files' => TRUE automatically sets the enctype in the Form tag; -->
+<!-- DISPALY ERROR MESSAGES, IF ANY: -->
+<?php $messages = $errors->all('<p style="color:red">:message</p>') ?>
+<?php
+foreach ($messages as $msg){
+  echo $msg;
+}
+// <!-- CREATE A FORM: -->
+// <!-- 'files' => TRUE automatically sets the enctype in the Form tag; -->
 <?= Form::open(array('files' => TRUE)) ?>
   
-  <?= Form::label('myfile', 'My File') ?>
+  <?= Form::label('myfile', 'Choose File (PDF or JPG only)') ?>
   <?= Form::file('myfile') ?>
   <br/>
   <?= Form::submit('Upload') ?>
