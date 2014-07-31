2caf8de20150a11ef5caf3e72413a7fe69f70342
Rachel Loziuk <rachel.loziuk@gmail.com>
2014-07-31 16:45:53 -0500

    add 3 remeber-token methods to User model

--- app/models/User.php
+++ app/models/User.php
@@ -16,6 +16,21 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
 	 */
 	protected $table = 'users';
 
+  public function getRememberToken()
+{
+    return $this->remember_token;
+}
+
+public function setRememberToken($value)
+{
+    $this->remember_token = $value;
+}
+
+public function getRememberTokenName()
+{
+    return 'remember_token';
+}
+
 	/**
 	 * The attributes excluded from the model's JSON form.
 	 *
